fd537ea639c78943b9c828e2c5702767921581de
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-03-24 10:24:03 -0300

    Capturando o titulo do produto

--- src/utils/browser.py
+++ src/utils/browser.py
@@ -1,8 +1,10 @@
+from math import prod
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 from time import sleep
 
 # Caminho para a raiz do projeto
@@ -28,6 +30,39 @@ class BrowserChrome:
             options=self.chrome_options
         )
 
+    def raspa_dados_dos_produtos(self):
+        try:
+            self.rolar_pagina()
+            html_soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+            # lista_produtos = html_soup.select("div[id='gridItemRoot']")
+            lista_produtos = html_soup.select('#gridItemRoot > div')
+            print(f'N° produtos: {len(lista_produtos)}')
+            for produto in lista_produtos:
+                titulo = produto.select(
+                    '#gridItemRoot > div > div.zg-grid-general-faceout > div > a:nth-child(2) > span > div')[0]
+                titulo = titulo.get_text()
+                # titulo = self.limpar_titulo(titulo)
+                print(titulo)
+        except Exception as e:
+            print('ERRO:', e)
+    
+    def limpar_titulo(self, titulo):
+        if ':' in titulo:
+            titulo = titulo.split(':')[0]
+        if '-' in titulo:
+            titulo = titulo.split('-')[0]
+        if '|' in titulo:
+            titulo = titulo.split('|')[0]
+        if '–' in titulo:
+            titulo = titulo.split('–')[0]
+
+        return titulo
+        
+    def rolar_pagina(self):
+        page = self.browser.find_element(By.TAG_NAME, 'html')
+        page.send_keys(Keys.END)
+        sleep(4)
+
     def acessa(self, link):
         self.browser.get(link)
 
@@ -38,5 +73,6 @@ class BrowserChrome:
 if __name__ == '__main__':
     drive = BrowserChrome('--disable-gpu', '--no-sandbox')
     drive.acessa('https://www.amazon.com.br/gp/bestsellers/electronics')
+    drive.raspa_dados_dos_produtos()
     sleep(4)
     drive.sair()
