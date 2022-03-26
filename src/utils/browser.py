4c1f0b9e85d1168141a9bf752c581b3bb89252cb
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-03-26 09:00:49 -0300

    Passeando por todas as paginas

--- src/utils/browser.py
+++ src/utils/browser.py
@@ -31,6 +31,11 @@ class BrowserAuto:
             options=self.chrome_options
         )
 
+    def scraping(self):
+        lista_dados = self.raspa_dados_dos_produtos()
+        self.dataframe_para_exel(self.gerar_data_frame(lista_dados))
+        self.sair()
+
     def raspa_dados_dos_produtos(self):
         lista_dados_produtos = {}
         try:
@@ -40,21 +45,30 @@ class BrowserAuto:
                     '#gridItemRoot > div > div.zg-grid-general-faceout > div > a:nth-child(2) > span > div'
                 )[0].get_text()
 
-                preco = produto.select(
-                    '#gridItemRoot > div > div.zg-grid-general-faceout > div > div:nth-child(4) > a > span > span'
-                )[0].get_text()
-
+                try:
+                    preco = produto.select(
+                        '#gridItemRoot > div > div.zg-grid-general-faceout > div > div:nth-child(4) > a > span > span'
+                    )[0].get_text()
+                except:
+                    preco = 'R$ --------'
+                
                 link = 'https://www.amazon.com.br' + produto.select_one(
                     '#gridItemRoot > div > div.zg-grid-general-faceout > div > a:nth-child(2)'
                 ).get('href')
 
                 lista_dados_produtos[f'#{n}'] = [titulo, preco, link]
-                # titulo = self.limpar_titulo(titulo)
+
         except Exception as e:
-            print('ERRO:', e)
+            print('Erro Na captura dos dados:', e)
 
         return lista_dados_produtos
-    
+
+    def proxima_pagina(self):
+        try:
+            self.browser.find_element(By.CLASS_NAME, 'a-last').click()
+        except Exception as e:
+            print(f'Erro ao passar pagina: {e}')
+
     def dataframe_para_exel(self, dataframe):
         dataframe.to_excel('Produtos.xlsx')
 
@@ -64,9 +78,13 @@ class BrowserAuto:
         return data_frame
 
     def obter_lista_produtos(self):
-        self.rolar_pagina()
-        html_soup = BeautifulSoup(self.browser.page_source, 'html.parser')
-        lista_produtos = html_soup.select('#gridItemRoot > div')
+        lista_produtos = []
+        for n in range(2):
+            self.rolar_pagina()
+            html_soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+            lista_produtos.extend(html_soup.select('#gridItemRoot > div'))
+            self.proxima_pagina()
+        
         return lista_produtos
 
     def limpar_titulo(self, titulo):
@@ -96,7 +114,4 @@ class BrowserAuto:
 if __name__ == '__main__':
     drive = BrowserAuto('--disable-gpu', '--no-sandbox')
     drive.acessa('https://www.amazon.com.br/gp/bestsellers/electronics')
-    produtos = drive.raspa_dados_dos_produtos()
-    drive.dataframe_para_exel(drive.gerar_data_frame(produtos))
-    sleep(4)
-    drive.sair()
+    drive.scraping()
